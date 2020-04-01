222b757d6d1bb87eb446a7655d93913b52b1f33a
nyanloutre <paul@nyanlout.re>
2020-04-01 23:54:29 +0200

    mastodon: init from PR 78810

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15408,6 +15408,8 @@ in
     };
   }).pkgs; toPythonApplication mailman-web;
 
+  mastodon = callPackage ../servers/mastodon { };
+
   mattermost = callPackage ../servers/mattermost { };
   matterircd = callPackage ../servers/mattermost/matterircd.nix { };
   matterbridge = callPackage ../servers/matterbridge { };
