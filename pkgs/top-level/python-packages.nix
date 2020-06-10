20da02dbe8945a0bb898ad0fa978796ae5304661
nyanloutre <paul@nyanlout.re>
2020-06-10 19:38:40 +0200

    tidal-dl: init at 2020.5.19.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1664,6 +1664,8 @@ in {
 
   affine = callPackage ../development/python-modules/affine { };
 
+  aigpy = callPackage ../development/python-modules/aigpy { };
+
   aioconsole = callPackage ../development/python-modules/aioconsole { };
 
   aiodns = callPackage ../development/python-modules/aiodns { };
@@ -5932,6 +5934,8 @@ in {
   testresources = callPackage ../development/python-modules/testresources { };
 
   testtools = callPackage ../development/python-modules/testtools { };
+
+  tidal-dl = callPackage ../development/python-modules/tidal-dl { };
 
   traitlets = callPackage ../development/python-modules/traitlets { };
 
