d1eab0ad53a124c52a80c4418f489e1adbcaf579
Janne Heß <janne@hess.ooo>
2020-11-02 23:35:34 +0100

    nixos/promtail: Add a promtail module
    (cherry picked from commit 54217cac698fdcf5c047e53054b7d3b039337f02)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -390,6 +390,7 @@
   ./services/logging/logcheck.nix
   ./services/logging/logrotate.nix
   ./services/logging/logstash.nix
+  ./services/logging/promtail.nix
   ./services/logging/rsyslogd.nix
   ./services/logging/syslog-ng.nix
   ./services/logging/syslogd.nix
